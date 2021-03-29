b65d436db9195a658e482526969f2e58e43782c1
Cesar Nuñez <alejoo2310@gmail.com>
2021-03-29 07:47:22 -0500

    modificando la pagina calculator

--- src/app/tabs/calculator/calculator.page.ts
+++ src/app/tabs/calculator/calculator.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-calculator',
@@ -6,18 +7,18 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./calculator.page.scss'],
 })
 export class CalculatorPage implements OnInit {
-  history = [];
   resultText: String = '';
   @ViewChild('form') form: ElementRef;
   @ViewChild('resInput') resInput: ElementRef;
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
   }
 
   resultado(res) {
-    this.history.push(res);
-    console.log(this.history);
+    this.appService.history.push(this.resInput.nativeElement.value);
+    this.resInput.nativeElement.value = eval(this.resInput.nativeElement.value);
+    this.appService.history.push(this.resInput.nativeElement.value);
   }
 
 }
